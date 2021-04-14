c4711282f4fdd4c7c1ef0ea72f4b69a78e64f151
Ivan L. Seibel <me@ivanseibel.dev>
2021-04-13 21:14:24 -0300

    feat: add models Category and Auction

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,29 @@
+import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=64)
+
+class Auction(models.Model):
+    id = models.IntegerField
+    title = models.CharField(max_length=64)
+    description = models.TextField(max_length=255)
+    starting_bid = models.DecimalField(max_digits=6, decimal_places=2)
+    photo_url = models.URLField(max_length=255)
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.SET_NULL, 
+        null=True
+    )
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
