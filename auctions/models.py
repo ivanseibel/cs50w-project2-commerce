7863ad186affdb481de10ecbf415152ebf08d590
Ivan L. Seibel <me@ivanseibel.dev>
2021-04-16 20:20:12 -0300

    feat: add new models

--- auctions/models.py
+++ auctions/models.py
@@ -1,18 +1,24 @@
 import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.timezone import now
 
 
 class User(AbstractUser):
     pass
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 class Auction(models.Model):
-    id = models.IntegerField
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=64)
     description = models.TextField(max_length=255)
     starting_bid = models.DecimalField(max_digits=6, decimal_places=2)
@@ -27,3 +33,34 @@ class Auction(models.Model):
         on_delete=models.CASCADE
     )
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Bid(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    value = models.DecimalField(max_digits=6, decimal_places=2)
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE
+    ),
+    auction = models.ForeignKey(
+        'Auction',
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    text = models.TextField(max_length=255)
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE
+    ),
+    auction = models.ForeignKey(
+        'Auction',
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
