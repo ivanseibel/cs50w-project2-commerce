785e7c22152850d7bb58ba64ee955b3b56b6d910
Ivan L. Seibel <me@ivanseibel.dev>
2021-04-18 23:03:34 -0300

    chore: update models

--- auctions/models.py
+++ auctions/models.py
@@ -12,17 +12,17 @@ class User(AbstractUser):
 
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    objects = models.Manager()
 
 class Auction(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=64)
-    description = models.TextField(max_length=255)
+    title = models.CharField(max_length=64, null=False)
+    description = models.TextField(max_length=255, null=False)
     starting_bid = models.DecimalField(max_digits=6, decimal_places=2)
-    photo_url = models.URLField(max_length=255)
+    photo_url = models.URLField(max_length=255, null=False)
     category = models.ForeignKey(
         'Category',
         on_delete=models.SET_NULL, 
@@ -53,7 +53,7 @@ class Bid(models.Model):
 
 class Comment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    text = models.TextField(max_length=255)
+    text = models.TextField(max_length=255, null=False)
     user = models.ForeignKey(
         'User',
         on_delete=models.CASCADE
