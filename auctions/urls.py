bfb03eb97f6a897c30f235d52f8ba565074c111f
Ivan L. Seibel <me@ivanseibel.dev>
2021-04-21 19:13:51 -0300

    feat: add update_auction endpoint

--- auctions/urls.py
+++ auctions/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("create_auction", views.create_auction, name="create_auction"),
-    path("update_auction", views.update_auction, name="update_auction"),
+    path("update_auction/<str:auction_id>", views.update_auction, name="update_auction"),
 ]
